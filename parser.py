e8a286837a4a2e43fe8d364a2d5f581d177db84a
stacy-eliz <34184142+stacy-eliz@users.noreply.github.com>
2020-08-14 18:07:31 +0400

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,4 +1,5 @@
 import openpyxl
+import generator
 
 wb = openpyxl.load_workbook('Приложение №2.xlsx')
 sheets = wb.sheetnames
@@ -24,59 +25,24 @@ count_of_teacher = []
 count_of_classroom = []
 number_of_program = []
 
+auditoriums = []
+programms = []
 for j in range(2):
     if j == 0:
         sheet = wb['параметры аудиторий']
-        t = None
-        for i in ['A', 'B', 'C', 'D', 'E', 'F']:
-            for j in range(2, 13):
-                if i == 'A':
-                    t = name1
-                elif i == 'B':
-                    t = places
-                elif i == 'C':
-                    t = type_of_discipline
-                elif i == 'D':
-                    t = differences
-                elif i == 'E':
-                    t = prioritet_discipline
-                elif i == 'F':
-                    t = anti_discipline
-                t.append(sheet['{}{}'.format(i, j)].value)
+        for i in range(2, 13):
+            auditoriums.append(
+                generator.ClassRoom(sheet['A{}'.format(i)], sheet['B{}'.format(i)], sheet['C{}'.format(i)],
+                                    sheet['D{}'.format(i)], sheet['E{}'.format(i)], sheet['F{}'.format(i)]))
     elif j == 1:
         sheet = wb['параметры программ']
-        t = None
-        for i in ['B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N']:
-            for j in range(2, 42):
-                if i == 'B':
-                    t = discipline
-                elif i == 'C':
-                    t = curriculum
-                elif i == 'D':
-                    t = features
-                elif i == 'E':
-                    t = hours
-                elif i == 'F':
-                    t = hours_proizv
-                elif i == 'G':
-                    t = hours_distant
-                elif i == 'H':
-                    t = day_offline
-                elif i == 'I':
-                    t = count_of_groups
-                elif i == 'J':
-                    t = count_of_worker
-                elif i == 'K':
-                    t = count_of_listener
-                elif i == 'L':
-                    t = count_of_teacher
-                elif i == 'M':
-                    t = count_of_classroom
-                elif i == 'N':
-                    t = number_of_program
-                x = sheet['{}{}'.format(i, j)].value
-                if x:
-                    x = str(x).replace('\n', '')
-                else:
-                    x = t[-1]
-                t.append(x)
+        x_ = sheet['B2'].value
+        for i in range(2, 42):
+            x = sheet['B{}'.format(i)].value
+            if x:
+                x = str(x).replace('\n', '')
+            programms.append(
+                generator.Program(sheet['A{}'.format(i)], x, sheet['C{}'.format(i)], sheet['D{}'.format(i)],
+                                  sheet['E{}'.format(i)], sheet['F{}'.format(i)], sheet['G{}'.format(i)],
+                                  sheet['H{}'.format(i)], sheet['I{}'.format(i)], sheet['J{}'.format(i)],
+                                  sheet['K{}'.format(i)], sheet['L{}'.format(i)], sheet['M{}'.format(i)]))
