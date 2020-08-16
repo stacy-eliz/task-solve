fa2eaa2c4b785b3e635845dbf57495bb0d976066
LogachevaSophia <48216113+LogachevaSophia@users.noreply.github.com>
2020-08-16 10:27:44 +0300

    Add files via upload

--- generator.py
+++ generator.py
@@ -3,6 +3,18 @@ from parser2 import application2_audit, application2_programm
 from parser1 import application1
 from reading_teachers import Full_teacher
 from random import shuffle
+from openpyxl import Workbook
+from openpyxl import load_workbook
+wb = load_workbook("output.xlsx")
+ws1 = wb["ДПО"]
+def room_xls(l,s, st):
+    if s!=0:
+        audit = st + " "+ str(s.name)       
+        ws1.cell(row=l[1]+1, column=l[0]+7,value=audit)
+def save():
+    wb.save("output.xlsx")
+
+
 
 Shedule1 = classes.Shedule()
 path_app1 = "app1.xlsx"
@@ -124,7 +136,8 @@ for i in range(1, len(a)):
                             if len(f) != 0:
                                 for s in f:
                                     s.is_busy = 1
-                                    Shedule1.Add_Room(s, a[i][2], None, b[k].name)
+                                    Shedule1.Add_Room(s, a[i][2], None, b[k].programme,[i,j])
+                                    room_xls([i,j], s, a[i][j])
                                     break
 
 
@@ -141,14 +154,17 @@ for i in range(1, len(a)):
                                         if len(f) != 0:
                                             for s in f:
                                                 s.is_busy = 1
-                                                Shedule1.Add_Room(s, a[i][2], None, b[k].name)
+                                                Shedule1.Add_Room(s, a[i][2], None, b[k].programme,[i,j])
                                                 Shedule1.swap_rooms(c[l], s, a[i][2])
+                                                room_xls([i,j], s, a[i][j])
                                                 break
                                     else:
-                                        Shedule1.Add_Room(c[l], a[i][2], None, a[i][j])
+                                        Shedule1.Add_Room(c[l], a[i][2], None, b[k].programme, [i,j])
+                                        room_xls([i,j], s, a[i][j])
                                 elif h in c[l].differences.lower() and not (c[l].is_busy):
 
-                                    Shedule1.Add_Room(c[l], a[i][2], None, a[i][j])
+                                    Shedule1.Add_Room(c[l], a[i][2], None,  b[k].programme,[i,j])
+                                    room_xls([i,j], s, a[i][j])
                                     c[l].is_busy = 1
                                     break
         elif type(a[i][j]) is list:
@@ -166,13 +182,16 @@ for i in range(1, len(a)):
                                         if len(f) != 0:
                                             for s in f:
                                                 s.is_busy = 1
-                                                Shedule1.Add_Room(s, a[i][2], None, b[k].name)
+                                                Shedule1.Add_Room(s, a[i][2], None, b[k].programme,[i,j])
                                                 Shedule1.swap_rooms(c[l], s, a[i][2])
+                                                room_xls([i,j], s, "")
                                                 break
                                     else:
-                                        Shedule1.Add_Room(c[l], a[i][2], None, a[i][j][g])
+                                        Shedule1.Add_Room(c[l], a[i][2], None,b[k].programme, [i,j])
+                                        room_xls([i,j], s, "")
                                 elif h in c[l].differences.lower() and not (c[l].is_busy):
-                                    Shedule1.Add_Room(c[l], a[i][2], None, a[i][j][g])
+                                    Shedule1.Add_Room(c[l], a[i][2], None, b[k].programme, [i,j])
+                                    room_xls([i,j], s, "")
                                     c[l].is_busy = 1
                                     break
 
@@ -187,192 +206,13 @@ for i in range(1, len(a)):
         count_free_room_weeks = 0
 
     Shedule1.leave_rooms()
-import classes
-from parser2 import application2_audit, application2_programm
-from parser1 import application1
-from reading_teachers import Full_teacher
-from random import shuffle
 
-Shedule1 = classes.Shedule()
-path_app1 = "app1.xlsx"
-a = application1(path_app1)
-path = "app2.xlsx"
-b = application2_programm(path)  # массив объектов программ
-c = application2_audit(path)  # массив объектов аудиторий
-s = 0
-
-counter_weeks = 0
-count_free_room_weeks = 0
-
-count_free_room_week = 0
-
-y1 = []
-y2 = []
-
-y3 = []
-y4 = []
+save()
+##for i in Shedule1.busy_room:
+##    print("1")
+##    print(i, Shedule1.busy_room[i])
+##print(len(Shedule1.busy_room))
+##print(len(Shedule1.busy_teachers))
+    
 
 
-def shorter(st, f):
-    if f:
-        for i in range(len(st)):
-            if st[i].isnumeric():
-                return st[:i]
-    else:
-        for i in range(len(st)):
-            if st[i].isnumeric():
-                return st[i:][:11]
-    return st
-
-
-teachers = Full_teacher('app2.xlsx')
-flag = False
-flag2 = False
-count = 0
-unic = []
-for i in range(1, len(a)):
-    flag1 = True
-    for j in range(len(a[i])):
-        if flag1 and a[i][j] != None and 'Неделя' in a[i][j]:
-            count += 1
-            flag1 = False
-        elif a[i][j] != None and not 'Неделя' in a[i][j]:
-            if a[i][j] not in unic:
-                unic.append(a[i][j])
-                if isinstance(a[i][j], list):
-                    # print(a[i][j], count)
-                    for k in a[i][j]:
-                        shuffle(teachers)
-                        for m in teachers:
-                            x = m.disciplin.replace('Управление безопасностью полетов', 'Безопасность полетов').replace(
-                                'Организация пассажирских перевозок', 'Пассажирские перевозки')
-                            if x in a[0][j] or a[0][j] in x:
-                                if not m.is_busy:
-                                    Shedule1.Add_Teacher(m, shorter(k, 0), x, k, count)
-                                    m.is_busy = 1
-                                    flag2 = True
-                                    break
-                        if flag2:
-                            flag2 = False
-                            break
-                else:
-                    shuffle(teachers)
-                    for m in teachers:
-                        x = m.disciplin.replace('Управление безопасностью полетов', 'Авиационная безопасность').replace(
-                            'Организация пассажирских перевозок', 'Пассажирские перевозки')
-                        if x in a[0][j] or a[0][j] in x:
-                            if not m.is_busy:
-                                Shedule1.Add_Teacher(m, shorter(a[i][j], 0), x, a[i][j], count)
-                                m.is_busy = 1
-                                flag = True
-                                break
-                    if flag:
-                        flag = False
-                        break
-    Shedule1.leave_teachers()
-
-del unic
-# for j in Shedule1.busy_teachers:
-#     print(j, Shedule1.busy_teachers[j])
-
-teacher_time = {}
-cons = 1
-for j in Shedule1.busy_teachers:
-    n = j[0]  # имя препода
-    w = Shedule1.busy_teachers[j][-1]  # номер недели
-    a = Shedule1.busy_teachers[j][-2]
-    a = a.split()
-    if a[2][3:5] != a[2][-2:]:
-        cons += int(a[2][6:8])
-        cons += 30 - int(a[2][:2])
-        cons += 1
-    else:
-        cons = int(a[2][6:8]) - int(a[2][:2]) + 1
-    if n in teacher_time:
-        if w in teacher_time[n]:
-            teacher_time[n][w] += cons
-        else:
-            teacher_time[n][w] = cons
-    else:
-        teacher_time[n] = {w: cons}
-
-a = application1(path_app1)
-for i in range(1, len(a)):
-    counter_weeks += 1
-    for j in range(len(a[i])):
-        if not (type(a[i][j]) is list) and a[i][j] != None and a[0][j] != None:
-            for k in range(len(b)):
-                if "Опасные грузы" in a[0][j]:
-                    cate = a[0][j].replace("Опасные грузы.", "").replace(" категория", "")
-                    if "опасные грузы" in b[k].name.lower():
-                        cate += " категория ИКАО/ИАТА"
-                        if "Базовый" in a[i][j]:
-                            cate += ".базовый курс"
-                        if cate.lower() in b[k].programme.lower() or b[k].programme.lower() in cate.lower():
-                            f = Shedule1.get_free_rooms()
-                            if len(f) != 0:
-                                for s in f:
-                                    s.is_busy = 1
-                                    Shedule1.Add_Room(s, a[i][2], None, b[k].name)
-                                    break
-
-
-                else:
-                    d = shorter(a[i][j], True).replace("Повышение", "повышения")
-                    d = d.lower()
-                    if a[0][j].lower() in b[k].name.lower() or b[k].name.lower() in a[0][j].lower():
-                        if d in b[k].programme.lower() or b[k].programme.lower() in d:
-                            for l in range(len(c)):
-                                h = b[k].features.lower().replace("требуется ", "").replace("нет", "")
-                                if str(c[l].name) in h:
-                                    if c[l].is_busy:
-                                        f = Shedule1.get_free_rooms()
-                                        if len(f) != 0:
-                                            for s in f:
-                                                s.is_busy = 1
-                                                Shedule1.Add_Room(s, a[i][2], None, b[k].name)
-                                                Shedule1.swap_rooms(c[l], s, a[i][2])
-                                                break
-                                    else:
-                                        Shedule1.Add_Room(c[l], a[i][2], None, a[i][j])
-                                elif h in c[l].differences.lower() and not (c[l].is_busy):
-
-                                    Shedule1.Add_Room(c[l], a[i][2], None, a[i][j])
-                                    c[l].is_busy = 1
-                                    break
-        elif type(a[i][j]) is list:
-            for g in range(len(a[i][j])):
-                for k in range(len(b)):
-                    d = shorter(a[i][j][g], True).replace("Повышение", "повышения")
-                    d = d.lower()
-                    if a[0][j][g].lower() in b[k].name.lower() or b[k].name.lower() in a[0][j][g].lower():
-                        if d in b[k].programme.lower() or b[k].programme.lower() in d:
-                            for l in range(len(c)):
-                                h = b[k].features.lower().replace("требуется ", "").replace("нет", "")
-                                if str(c[l].name) in h:
-                                    if c[l].is_busy:
-                                        f = Shedule1.get_free_rooms()
-                                        if len(f) != 0:
-                                            for s in f:
-                                                s.is_busy = 1
-                                                Shedule1.Add_Room(s, a[i][2], None, b[k].name)
-                                                Shedule1.swap_rooms(c[l], s, a[i][2])
-                                                break
-                                    else:
-                                        Shedule1.Add_Room(c[l], a[i][2], None, a[i][j][g])
-                                elif h in c[l].differences.lower() and not (c[l].is_busy):
-                                    Shedule1.Add_Room(c[l], a[i][2], None, a[i][j][g])
-                                    c[l].is_busy = 1
-                                    break
-
-    if i % 2 == 0:
-        count_free_room_week = len(Shedule1.get_free_rooms())
-        y2.append(count_free_room_week / 11 * 100)
-
-    count_free_room_weeks += len(Shedule1.get_free_rooms())
-    if counter_weeks == 4:
-        y1.append(count_free_room_weeks / 44 * 100)
-        counter_weeks = 0
-        count_free_room_weeks = 0
-
-    Shedule1.leave_rooms()
